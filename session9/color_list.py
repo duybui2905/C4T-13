42e9e72bd2efa50d6ce0b6bb43ac6e53194f4243
duybui2905 <buinangduy1@gmail.com>
2019-03-31 16:26:19 +0700

    session9 done

--- session9/color_list.py
+++ session9/color_list.py
@@ -6,4 +6,12 @@ color.append(new_color)
 print("the new color list: ")
 print(*color, sep = ", ")
 place = int(input("enter a position: "))
-print("color at position:", )
+print("color at position:", color[place])
+for i, item in enumerate(color):
+    print(i+1, ".", item)
+color_to_delete = input("enter the color you want to delete (value or position): ")
+if color_to_delete.isdigit():
+    color.pop(int(color_to_delete))
+else:
+    color.remove(color_to_delete)
+print(color)
