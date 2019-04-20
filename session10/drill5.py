62baa0e52d9a56631aae9da04e532e162459114c
duybui2905 <buinangduy1@gmail.com>
2019-04-20 10:34:18 +0700

    session10 drill 5

---
+++ session10/drill5.py
@@ -0,0 +1,8 @@
+restaurant ={
+    "Huy" : ["waiter", 12, 0.8],
+    "Tung" : ["cook", 24, 1.5],
+    "M.Duc" : ["mananger", 20, 2],
+    "Don" : ["waiter", 12, 0.9],
+    "H.Duc" : ["waiter", 14, 0.7],
+}
+print(restaurant)
