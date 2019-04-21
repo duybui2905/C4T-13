3a241155ad0c289ee4c1865eb7f2fdb2517df88a
duybui2905 <buinangduy1@gmail.com>
2019-04-21 15:59:26 +0700

    session10

--- session10/my_movie.py
+++ session10/my_movie.py
@@ -4,6 +4,26 @@ my_mov = {
     'director' : 'Christopher Nolan',
     'year' : 2014,
 }
-print(my_mov)
+# print(my_mov)
 my_mov['country'] = 'the US'
-print(my_mov)
+# print(my_mov)
+for k,v in my_mov.items():
+    print(k, '-', v)
+
+# my_mov['actors'] = [1, 2, 3, 4]
+# print(my_mov['actors'])
+
+actors = my_mov['actors']
+# actors.append('dfedfrd')
+# print(my_mov['actors'])
+
+# actors.pop(0)
+# print(my_mov['actors'])
+
+# print(actors[1])
+
+# for i in actors:
+#     print(i)
+
+for k,v in my_mov.items():
+    print(k, ':', v)
